446ec478412c98eca662fa2693d20c5941330cfa
GeorgeLondon1 <radac.george@yahoo.com>
2021-02-13 16:56:31 +0000

    JD Ticketing Project - Rest

--- src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -1,30 +1,58 @@
 package com.cybertek.implementation;
 
+import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.User;
 import com.cybertek.entity.common.UserPrincipal;
+import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.UserRepository;
 import com.cybertek.service.SecurityService;
+import com.cybertek.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
-    private UserRepository userRepository;
+    private UserService userService;
+    private UserMapper userMapper;
 
-    public SecurityServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public SecurityServiceImpl(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
-        User user = userRepository.findByUserName(s);
+        UserDTO user = userService.findByUserName(s);
         if(user==null){
             throw new UsernameNotFoundException("This user does not exist");
         }
 
-        return new UserPrincipal(user);
+        return new org.springframework.security.core.userdetails.User(user.getId().toString(),
+                user.getPassWord(), listAuthorities(user));
+    }
+
+    @Override
+    public User loadUser(String param) {
+        UserDTO user = userService.findByUserName(param);
+        return userMapper.convertToEntity(user);
+    }
+
+    private Collection<? extends GrantedAuthority> listAuthorities(UserDTO user){
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getDescription());
+        authorityList.add(authority);
+
+        return authorityList;
     }
 }
