17279234180f9779a620149331df048772e84efa
GeorgeLondon1 <radac.george@yahoo.com>
2021-02-13 19:34:51 +0000

    JD Ticketing Project - Rest

--- src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
+++ src/main/java/com/cybertek/exception/ExceptionMessageHandler.java
@@ -1,5 +1,6 @@
 package com.cybertek.exception;
 
+import com.cybertek.annotation.DefaultExceptionMessage;
 import com.cybertek.dto.DefaultExceptionMessageDto;
 import com.cybertek.entity.ResponseWrapper;
 import org.springframework.core.Ordered;
